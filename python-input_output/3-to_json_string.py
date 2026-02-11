83247b8b1563d06956590f74750038b4c9f68d6c
DenZzz1 <m2nky2fficiel@gmail.com>
2026-02-11 12:39:30 +0000

    add 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a function to convert object to JSON string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object (string)
+
+    Args:
+        my_obj: object to serialize to JSON
+
+    Returns:
+        JSON string representation of the object
+    """
+    return json.dumps(my_obj)
