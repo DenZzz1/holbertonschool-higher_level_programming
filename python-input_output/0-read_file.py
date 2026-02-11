63ab86ae90791bda374a2787a265146ef17a9056
DenZzz1 <m2nky2fficiel@gmail.com>
2026-02-11 12:26:52 +0000

    add 0-read_file.py and readme.md

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a function to read a file"""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout
+
+    Args:
+        filename: name of the file to read (default: empty string)
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
