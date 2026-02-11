faac2b80426db902697c1b2c8e8da83618e1e1af
DenZzz1 <m2nky2fficiel@gmail.com>
2026-02-11 12:37:49 +0000

    update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""Module that defines a function to append to a file"""
+
 
 def append_write(filename="", text=""):
+    """Append a string at the end of a text file (UTF8)
+
+    Args:
+        filename: name of the file to append to (default: empty string)
+        text: text to append to the file (default: empty string)
+ 
+    Returns:
+        The number of characters added
+    """
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
