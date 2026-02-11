04b9418b1a30a8f02145230cfb8460c386a6e753
DenZzz1 <m2nky2fficiel@gmail.com>
2026-02-11 18:47:13 +0000

    add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student:
+    """Student class with serialization and deserialization methods"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student
+
+        Args:
+            first_name: first name of the student
+            last_name: last name of the student
+            age: age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the Student instance
+
+        Args:
+            attrs: list of attribute names to retrieve (optional)
+                   If None, retrieve all attributes
+
+        Returns:
+            Dictionary with specified attributes (or all if attrs is None)
+        """
+        if attrs is None:
+            return self.__dict__
+
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance
+
+        Args:
+            json: dictionary with attribute names and values
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
